16e7c14b05d61fec510460d60071f42d580a6227
Spider X <zhaizhixuan070305@qq.com>
2026-09-20 17:23:05 +0800

    Create Permutations.cpp

---
+++ DP/Permutations.cpp
@@ -0,0 +1,21 @@
+//https://leetcode.cn/problems/permutations/description/
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        f(nums,0,ans);
+        return ans;
+    }
+    void f(vector<int>& nums,int i,vector<vector<int>>& ans){
+        if(i==nums.size()){
+            ans.push_back(nums);
+        }
+        else{
+            for(int j=i;j<nums.size();j++){
+                swap(nums[i],nums[j]);
+                f(nums,i+1,ans);
+                swap(nums[i],nums[j]);
+            }
+        }
+    }
+};
